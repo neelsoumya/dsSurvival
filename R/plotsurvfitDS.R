9441eb43ace3213e0ec43962ef9e918b0ab7f768
Soumya Banerjee <neel.soumya@gmail.com>
2022-03-08 14:27:01 +0000

    Update plotsurvfitDS.R

--- R/plotsurvfitDS.R
+++ R/plotsurvfitDS.R
@@ -201,17 +201,19 @@ plotsurvfitDS<-function(formula = NULL,
       # TODO: make it depend on number of data points on X axis 	
       f_span = 0.30	 # useable span 0.3-0.55
       smoothed_survfit = stats::loess(survfit_model_variable$surv ~ survfit_model_variable$time, span = f_span)	
-  #    
-  #    # predict
+      
+      # predict
       predict_smoothed_survfit = stats::predict(smoothed_survfit)  	
-  #    # TODO: modify last point and make sure not negative and not greater than previous point	
-  #    # assign to surv variable the smoothed data	
+      # TODO: modify last point and make sure not negative and not greater than previous point	
+      # assign to surv variable the smoothed data	
       survfit_model_variable$surv = predict_smoothed_survfit
   }
   # end if  	
 
 	
-	
+  ####################################
+  # Approach 3:	
+  # Smoothing option	
   ####################################
   #if (method_anonymization == 3) 	
   #{
