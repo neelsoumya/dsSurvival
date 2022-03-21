9e4d49db2166012923fd1232fc1f289cbe652f64
neelsoumya <neel.soumya@gmail.com>
2022-03-21 11:32:31 +0000

    new branch

--- R/plotsurvfitDS.R
+++ R/plotsurvfitDS.R
@@ -9,7 +9,7 @@
 #' @param formula a character string which has the name of server-side survfit() object.
 #'		This should be created using a call to ds.survfit()
 #' @param dataName character string of name of data frame
-#' @param method_anonymization an integer. Method of anonymization to be used (1: deterministic, 2: probabilistic, 3: smoothing). Default value is 2.
+#' @param method_anonymization an integer. Method of anonymization to be used (3: smoothing). Default value is 3.
 #' @param noise an integer. fraction of noise (between 0 and 1) to be added to original data. 
 #'     Noise is added as a percentage of original value. 
 #'     This is used for probabilistic anonymization.
@@ -21,7 +21,7 @@
 #' @export
 plotsurvfitDS<-function(formula = NULL,
                         dataName = NULL,
-			method_anonymization = 2,
+			method_anonymization = 3,
 			noise = 0.03,
 			knn = 20
                        )
@@ -65,133 +65,6 @@ plotsurvfitDS<-function(formula = NULL,
   survfit_model_variable = eval(parse(text=formula), envir = parent.frame())
   
   
-  ##############################################################
-  # if probabilistic anonymization then generate and add noise	
-  ##############################################################
-  if (method_anonymization == 2)
-  {	      
-    
-    # set study specific seed
-    seed <- getOption("datashield.seed")
-    if (is.null(seed))
-    {
-      stop("plotsurvfitDS requires a seed to be set and requires 'datashield.seed' R option to operate", call.=FALSE)
-    }
-    
-    # if there is a seed, then set it
-    set.seed(seed)	
-    
-	  
-    # check if percentage of noise greater than threshold   	  
-    f_noise_threshold = 0.001	  
-    if (noise < f_noise_threshold)
-    {
-	 stop( paste0(" 'noise' must be greater than or equal to ", f_noise_threshold),   call.=FALSE ) 
-    }
-    else
-    {
-	 percentage <- noise   
-    }	    
-	  
-    
-    ##########################################
-    # Approach 1: add noise before plotting
-    #
-    # add noise to:
-    # surv (i.e. proportion surviving)
-    # time (times at which events occur, ie when the proportion changes)
-    # this is for the y axis
-    # and for time on x axis
-    ##########################################
-    for ( i_temp_counter_inner in c(2:length(survfit_model_variable$surv)) )
-    {
-	      
-	      # generate noise absolute value so alsways positive
-	      # delta_noise_time <- abs( stats::rnorm(n = 1, mean = 0, sd = percentage) )
-	      # do not take absolute value but have it reject values if not monotonically increasing
-	      delta_noise_time <- stats::rnorm(n = 1, mean = 0, sd = percentage)	    
-	      # survfit_model_variable$time[i_temp_counter_inner] <- survfit_model_variable$time[i_temp_counter_inner] + delta_noise
-
-	      # previous value time
-	      prev_value_temp_time <- survfit_model_variable$time[i_temp_counter_inner - 1]
-	      # current value temp time
-	      curr_value_temp_time <- survfit_model_variable$time[i_temp_counter_inner]
-
-	      # proposed value of time with noise added as a proportion and added on top of whatever value exists
-	      value_noise_time_proposed = curr_value_temp_time + (curr_value_temp_time*delta_noise_time)
-
-	      if (prev_value_temp_time <= value_noise_time_proposed)
-	      {
-		  # if previous value time is less then proposed value then ok 
-		  # since time is supposed to be increasing
-
-		  # set this value to current time
-		  survfit_model_variable$time[i_temp_counter_inner] = value_noise_time_proposed
-
-	      }
-	      else
-	      {
-		   # set to previous time value
-		   survfit_model_variable$time[i_temp_counter_inner] = prev_value_temp_time
-	      }	    
-	    
-	    
-	    
-    }
-    # end for loop	  
-    
-  }
-  # end if	
-  
-  ######################################################################
-  # Approach 2: 
-  # deterministic anonymization by Demetris Avraam and Paul Burton
-  ######################################################################
-  if (method_anonymization == 1)
-  {
-        
-        # check if knn is not less than a threshold (see plothistogramDS) 	  
-	if (knn < nfilter.kNN)
-        {
-              stop(paste0("'knn' must be greater than or equal to ", nfilter.kNN), call.=FALSE)  # nfilter.noise
-        }	  
-	  
-        # Step 1: Standardise the variable
-        time.standardised <- (survfit_model_variable$time-mean(survfit_model_variable$time))/stats::sd(survfit_model_variable$time)
-
-        # Step 2: Find the k-1 nearest neighbours of each data point
-        nearest <- RANN::nn2(time.standardised, k = knn)
-
-        # Step 3: Calculate the centroid of each n nearest data points
-        time.centroid <- matrix()
-        for (i in 1:length(survfit_model_variable$time))
-	{
-             time.centroid[i] <- mean(time.standardised[nearest$nn.idx[i,1:knn]])
-        }
-
-        # Step 4: Calculate the scaling factor
-        time.scalingFactor <- stats::sd(time.standardised)/stats::sd(time.centroid)
-
-        # Step 5: Apply the scaling factor to the centroids
-        time.masked <- time.centroid * time.scalingFactor
-
-        # Step 6: Shift the centroids back to the actual position and scale of the original data
-        SURVTIME_anon <- (time.masked * stats::sd(survfit_model_variable$time)) + mean(survfit_model_variable$time)
-
-        # modify time in survfit object (instead of original time)
-        survfit_model_variable$time <- SURVTIME_anon
-	 
-        # if time needs adjustment, if min is less than 0 for example
-        #  then adjust and translate time  
-	if ( min(survfit_model_variable$time) < 0 )
-	{
-	    survfit_model_variable$time = survfit_model_variable$time - min(survfit_model_variable$time) 	
-	}	
-	  
-  }  
-  # end if  
-	
-	
   ################################################
   # Approach 3:	
   # Smoothing option
