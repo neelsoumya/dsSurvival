c5b70ea4ce32b58fc3f8561a9a742e442c7b1bf4
Soumya Banerjee <neel.soumya@gmail.com>
2022-05-11 06:07:28 +0100

    Update set_random_seed_settings.R

--- tests/testthat/random/set_random_seed_settings.R
+++ tests/testthat/random/set_random_seed_settings.R
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
- 2 # Copyright (c) 2019-2020 University of Newcastle upon Tyne. All rights reserved.
- 3 #
- 4 # This program and the accompanying materials
- 5 # are made available under the terms of the GNU Public License v3.0.
- 6 #
- 7 # You should have received a copy of the GNU General Public License
- 8 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 9 #-------------------------------------------------------------------------------
-10 #
+
 11 # DataSHIELD random settings
 12 #
 
