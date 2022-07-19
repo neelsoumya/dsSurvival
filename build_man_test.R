3edbb30a4a96da005c45ecae7b6b4ea83dc09121
Tom Bishop <trpb2@cam.ac.uk>
2022-07-19 08:06:40 -0700

    undoing push to neelsoumya repo Revert "updating from neelsoumya repo"
    This reverts commit 52aaf6bd332f1350b5af6716d6551b6c9476f5dc, reversing
    changes made to 8e3ca929759ea722173c72c258fa381b03293645.

---
+++ build_man_test.R
@@ -0,0 +1,45 @@
+###########################################
+# Script to build manuals and test code
+#
+# Usage:
+#   R --no-save < build_man_test.R
+#
+###########################################
+
+###################
+# load libraries
+###################
+library(devtools)
+library(testthat)
+library(dsBase)
+library(dsBaseClient)
+require('DSI')
+require('DSOpal')
+library(dsSurvival)
+library(dsSurvivalClient)
+
+##################
+# build manuals
+##################
+devtools::build_manual()
+
+########################
+# update documentation
+########################
+devtools::check_man()
+
+##################
+# build vignettes
+##################
+devtools::build_vignettes()
+
+##################
+# Testing
+##################
+devtools::test()
+
+##################
+# clean up
+##################
+gc()
+
