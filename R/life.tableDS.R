a6409c7335ea4ba69eee1c34a601ad727318c26c
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-05-25 17:15:41 +0200

    feat: new life.table method to pool survival analysis

---
+++ R/life.tableDS.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' @param survfit_object 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+life.tableDS <- function(survfit_object){
+  survfit_object_re <- summary(
+    survfit_object, times = seq(
+      plyr::round_any(min(survfit_object$time), 10, floor), 
+      plyr::round_any(max(survfit_object$time), 10, ceiling), 
+      10 # TODO This should be a function argument!
+    )
+  )
+  df <- data.frame(
+    time = survfit_object_re$time,
+    n.risk = survfit_object_re$n.risk,
+    n.event = survfit_object_re$n.event
+  )
+  if(!is.null(survfit_object_re$strata)){
+    split_vector <- survfit_object_re$strata
+    df_list <- split(df, split_vector)
+    names(df_list) <- levels(survfit_object_re$strata)
+  } else {
+    df_list <- list(df)
+    names(df_list) <- "life.table_no_strata"
+  }
+  return(df_list)
+}
